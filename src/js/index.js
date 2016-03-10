084e214871d4f8f116538720251b33cde7fcec54
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-10 17:02:15 -0500

    final commit thurs javascript almost working

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
 // Javascript Entry Point
+import etsyData from './items';
+import $ from 'jquery';
+
+var products = etsyData.results;
+
+function getProduct (product) {
+
+
+	var image 			= product.Images[0].url_170x135;
+	var description = product[0].description;
+	var price 			= product[0].price;
+	var title 			= product[0].title;
+	
+
+
+	return `
+		<div class="contentboxes">
+      <div class="box1">
+        <img src="${image}" alt="${description}">
+      </div>
+      <div class="price">${price}</div>
+      <div class="item-title">${title}</div>
+    </div>
+  `;
+}
+
+
+products.forEach(function(product){
+		var itemHtml = getProduct(product);
+		$('.contentboxescontainer').append(itemHtml);
+});
+
