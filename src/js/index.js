0ea1f7e38fbd8aae82ebce33921a6ccf80fa9ea7
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-03-11 12:17:41 -0500

    break for lunch. javascript working. putting touches on scss.

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,52 @@
-// Javascript Entry Point
+// // Javascript Entry Point
+// import etsyData from './items';
+// import $ from 'jquery';
+
+// var products = etsyData.results;
+
+// function getProduct (product) {
+
+
+// 	var image 			= product.Images[0].url_170x135;
+// 	var description = product[0].description;
+// 	var price 			= product[0].price;
+// 	var title 			= product[0].title;
+	
+
+
+// 	return `
+// 		<div class="contentboxes">
+//       <div class="box1">
+//         <img src="${image}" alt="${description}">
+//       </div>
+//       <div class="price">${price}</div>
+//       <div class="item-title">${title}</div>
+//     </div>
+//   `;
+// }
+
+
+// products.forEach(function(product){
+// 		var itemHtml = getProduct(product);
+// 		$('.contentboxescontainer').append(itemHtml);
+// });
+
+/////////////////
+
+
 import etsyData from './items';
 import $ from 'jquery';
 
 var products = etsyData.results;
 
-function getProduct (product) {
+var itemHtml = products.map(function(product) {
+
 
 
 	var image 			= product.Images[0].url_170x135;
-	var description = product[0].description;
-	var price 			= product[0].price;
-	var title 			= product[0].title;
+	var description = product.title;
+	var price 			= product.price;
+	var title 			= product.title;
 	
 
 
@@ -18,16 +54,14 @@ function getProduct (product) {
 		<div class="contentboxes">
       <div class="box1">
         <img src="${image}" alt="${description}">
-      </div>
-      <div class="price">${price}</div>
+        </div>
       <div class="item-title">${title}</div>
+      <div class="price">${price}</div>
     </div>
   `;
-}
+})
 
 
-products.forEach(function(product){
-		var itemHtml = getProduct(product);
 		$('.contentboxescontainer').append(itemHtml);
-});
+
 
